b800996d4049f733e321da1a43c7351b4bbaadcb
StepashaxGod <168844258+StepashaxGod@users.noreply.github.com>
2025-07-02 18:08:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ python3 -m venv venv
 source venv/bin/activate
 pip install flask flask-sqlalchemy cryptography werkzeug
 echo "Dependencies installed."
-echo "Virtual environment activated. Run 'python app.py' to start the application."
+echo "Virtual environment activated. Execute "source venv/bin/activate" and run app.py"
